c1fe18a8ed9d75cb73aacc3078dddcb7bf4be7f7
benji4411 <92875394+benji4411@users.noreply.github.com>
2022-11-02 15:57:58 +0100

    Update ChessAI.h

--- Engine/ChessAI.h
+++ Engine/ChessAI.h
@@ -5,9 +5,10 @@
 class ChessAI
 {
 public:
+	//Minimax algorithm with alpha-beta pruning
 	static std::pair<Move*, float> minimax(Board& board, int depth, float alpha, float beta, bool maximizingPlayer, bool maximizingColor);
 private:
-	static float evaluate(Board& board, bool maximizingColor);
+	static float evaluate(Board& board, bool maximizingColor);//Return the evaluation of a position
 
 };
 
